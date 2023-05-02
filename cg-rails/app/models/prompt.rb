f943a82c3d89b428dbed095d5a2df0eca7a11063
Fabricio Yukio TM <fabricioyukio@gmail.com>
2023-05-01 22:19:25 -0300

    removing master.key from versioning

--- cg-rails/app/models/prompt.rb
+++ cg-rails/app/models/prompt.rb
@@ -1,25 +1,2 @@
-# == Schema Information
-#
-# Table name: prompts
-# id             :bigint           not null, primary key
-# original_index :bigint          default(0), not null
-
 class Prompt < ApplicationRecord
-  validates :content, presence: true, length: {
-                                                minimum: 3,
-                                                maximum: 512
-                                              }
-
-  default_scope -> { order(created_at: :desc) }
-
-  searchkick
-
-  def search_data
-    {
-      name: name,
-      email: email,
-      total_blogposts: blogposts.count,
-      last_published_blogpost_date: last_published_blogpost_date
-    }
-  end
 end
--- cg-rails/config/routes.rb
+++ cg-rails/config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  resources :searches
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'page#home'
+
+  get 'about', to: 'pages#about'
+
+  resources :prompts
+
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
 end
